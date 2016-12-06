02930c6128b2382dec3225cd8a0e516f62b47f2f
azen moktan <azenmoktan22@gmail.com>
2016-12-07 02:48:00 +0545

    Navigation Menu Complete

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Website-Frontend-
+	
+	This is the project about making the frontend of the website
+
+
+# Technology Used
+
+	Html 5,Css , Javascript , Bootstrap
+
+
+
+#Author
+
+
+	Rajendra Tamang
---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>A Website template</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+
+</head>
+<body>
+<!-- mero nav bar start -->
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    <!-- azen respone -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <!-- end azen respone -->
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+
+
+
+    <!-- div for azen response collapse -->
+    <div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li><a href="#">Our Partners</a></li>
+      <li><a href="#">Our Team</a></li>
+    </ul>
+
+    <!-- mero search -->
+
+    <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+      </form>
+
+
+<!-- end mero search -->
+  </div>
+
+
+  </div>
+<!-- end azen response collapse -->
+</nav>
+<!-- mero nav bar end -->
+
+
+
+
+
+
+  <!-- start of container -->
+<div class="container">
+  <h1>My First Bootstrap Page</h1>
+  <p>This part is inside a .container class.</p> 
+  <p>The .container class provides a responsive fixed width container.</p>           
+
+
+
+<span>my name israjendra tamang</span>
+
+
+
+<table class="table">
+	<th>First</th>
+
+	<th>second</th>
+	<td>hari</td>
+	<td>hari</td>
+
+</table>
+
+
+
+
+<pre>azen moktan </pre>
+
+
+
+
+
+
+<!-- start of footer -->
+<div id="footer">
+	
+Copyright &copy Rajendra Tamang
+
+</div>
+<!-- end of footer -->
+
+
+
+</div>
+<!-- end of container -->
+</body>
+</html>
